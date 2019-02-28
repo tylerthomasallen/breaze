7554e97d764f6d2a35cc36cfb2b2c7bc62c8a7f4
Tyler <tylerthomasallen@gmail.com>
2019-02-28 15:43:42 -0800

    Add routes for users and giphs

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const users = require('./routes/api/users');
+const giphs = require('./routes/api/giphs');
 
 mongoose.connect(db, { useNewUrlParser: true })
 .then(() => console.log('Connected to MongoDB successfully'))
 .catch(err => console.log(err))
 
+app.use("/api/users", users);
+app.use("/api/giphs", giphs)
 app.get("/", (req, res) => res.send('Hello, Tyler'))
+
 
 const port = process.env.PORT || 5000;
 
