7e85e864d6ebadecb9d80cc651ae992333de15cb
Tyler <tylerthomasallen@gmail.com>
2019-02-28 20:58:23 -0800

    Add login

--- routes/api/users.js
+++ routes/api/users.js
@@ -28,8 +28,28 @@ router.post('/signup', (req, res) => {
         })
       }
     }
+  )
+})
 
-    )
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({ msg: 'Incorrect email'})
+      }
+
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            res.json({ msg: 'Success' })
+          } else {
+            return res.status(400).json({ msg: 'Incorrect password'})
+          }
+        })
+    })
 })
 
 module.exports = router;
