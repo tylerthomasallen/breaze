31ade346354e593bc139562bb6b01c41cf2bf9b6
Tyler <tylerthomasallen@gmail.com>
2019-02-28 20:51:42 -0800

    Add user signup

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 const db = require('./config/keys').mongoURI;
 const users = require('./routes/api/users');
 const giphs = require('./routes/api/giphs');
@@ -9,10 +11,12 @@ mongoose.connect(db, { useNewUrlParser: true })
 .then(() => console.log('Connected to MongoDB successfully'))
 .catch(err => console.log(err))
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use("/api/users", users);
 app.use("/api/giphs", giphs)
 app.get("/", (req, res) => res.send('Hello, Tyler'))
-
 
 const port = process.env.PORT || 5000;
 
