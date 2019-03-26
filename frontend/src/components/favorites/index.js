59d04b4ee997f12a8316a58fb91be8400e0ea9e7
Tyler <tylerthomasallen@gmail.com>
2019-03-26 11:34:25 -0700

    Add proptypes to favorites

--- frontend/src/components/favorites/index.js
+++ frontend/src/components/favorites/index.js
@@ -2,21 +2,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Giphs from '../giphs';
 import Title from '../title';
+import PropTypes from 'prop-types';
 
-const Favorites = ( { favorites } ) => {
+const Favorites = ( { array } ) => {
 
   return(
     <div className="parent">
       <Title text={'Favorites'} />
-      <Giphs giphs={favorites.array} />
+      <Giphs giphs={array} />
     </div>
   )
 }
 
-const mapStateToProps = ( { giphs: { favorites } }  ) => {
+const mapStateToProps = ( { giphs: { favorites: { array } } }  ) => {
   return {
-    favorites
+    array
   }
 }
+
+Favorites.propTypes = ({
+  array: PropTypes.array.isRequired
+})
 
 export default connect(mapStateToProps)(Favorites);
