be14f0bec2f0d1efc25c5731057cad523993cb82
Tyler <tylerthomasallen@gmail.com>
2019-03-26 11:39:38 -0700

    Add proptypes to giph loading

--- frontend/src/components/loading/giph_loading.js
+++ frontend/src/components/loading/giph_loading.js
@@ -1,29 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const GiphLoading = ( { loading } ) => {
-  
   if (!loading) {
-    
-    return null
-  
+    return null;
   } else {
-    
     return(
-      
       <div className="lds-css ng-scope">
-        
         <div className="lds-bars">
-
           <div />
           <div />
           <div />
           <div />
-
         </div>
-
       </div>
     )
   }
 }
+
+GiphLoading.propTypes = ({
+  loading: PropTypes.bool.isRequired
+})
 
 export default GiphLoading;
